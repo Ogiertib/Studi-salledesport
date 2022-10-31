75c3ec8d830f7d036697ac248e2d3c1399fe348f
Ogier <ogier9@gmail.com>
2022-10-31 15:50:23 +0100

    Lot update for fix bug

--- next.config.js
+++ next.config.js
@@ -2,10 +2,8 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  env: {
-		KEY_SENDGRID:
-			"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",
-	},
+  
+  
 }
 
 module.exports = nextConfig
